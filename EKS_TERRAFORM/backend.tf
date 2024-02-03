5350f6cf610681254c90f6f37a35a3785538e173
Chintan-Chaudhari <96288399+Chintan-Chaudhari@users.noreply.github.com>
2024-02-03 17:40:26 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mynew-cloudproject" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
+    key    = "EKS/pvtkey.tfstate"
     region = "ap-south-1"
   }
 }
